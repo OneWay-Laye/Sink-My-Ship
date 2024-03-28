08543489a23cd7dcab14fc4726ba5c6796cb3ef6
Alaye Taylor <tayloralaye@gmail.com>
2024-03-28 01:13:53 -0400

    This commit adds the vars to the main and creats Functions

---
+++ SMS-Functions.py
@@ -0,0 +1,8 @@
+# This will be the page that all of my functions are stored on
+def shipLocations():
+
+def printBoard():
+
+def checkChoice():
+
+def gameOver():
--- Sink-my-ship.py
+++ Sink-my-ship.py
@@ -1,1 +1,8 @@
 # This is the start of sink my ship
+
+# This will be the main fuction
+def main():
+    playerName = input("Enter Your name to get started!: ")
+    MISSTOTAL = 20
+    shotSpaces = []
+    
