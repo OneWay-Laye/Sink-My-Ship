be034f814a11a310ec727094a2b841f2cb4681da
Alaye Taylor <tayloralaye@gmail.com>
2024-03-28 01:35:23 -0400

    This commit creates the test ships and the ship file

--- SMS-Functions.py
+++ SMS-Functions.py
@@ -1,5 +1,6 @@
 # This will be the page that all of my functions are stored on
 def shipLocations():
+    infile = open('ship.txt', 'r')
 
 def printBoard():
 
--- Sink-my-ship.py
+++ Sink-my-ship.py
@@ -1,4 +1,6 @@
 # This is the start of sink my ship
+#This will import the functions used in the file.
+from SMS-Functions import shipLocations
 
 # This will be the main fuction
 def main():
